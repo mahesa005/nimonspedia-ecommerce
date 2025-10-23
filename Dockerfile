6fa75395a26eb5c6376d8baaa2b1bc2b40caed23
Muhammad Kinan Arkansyaddad <mkinanarkansyaddad@gmail.com>
2025-10-23 19:22:15 +0700

    feat: Initial project setup

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,9 @@
-FROM php:8.3-apache
-# add configuration here as needed
-RUN a2enmod rewrite
+FROM php:8.3-fpm-alpine
+
+RUN apk add --no-cache --update postgresql-dev
+
+RUN docker-php-ext-install pdo pdo_pgsql
+
+RUN echo "clear_env = no" >> /usr/local/etc/php-fpm.d/www.conf
+
+WORKDIR /var/www/html
---
+++ database/db_schema.sql
@@ -0,0 +1,127 @@
+CREATE TYPE user_role AS ENUM ('BUYER', 'SELLER');
+
+CREATE TYPE order_status AS ENUM (
+    'waiting_approval',
+    'approved',
+    'rejected',
+    'on_delivery',
+    'received'
+);
+
+CREATE TABLE "user" (
+    user_id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role user_role NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    address TEXT,
+    balance INT NOT NULL DEFAULT 0,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE "store" (
+    store_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL UNIQUE,
+    store_name VARCHAR(255) NOT NULL UNIQUE,
+    store_description TEXT,
+    store_logo_path VARCHAR(255),
+    balance INT NOT NULL DEFAULT 0,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+
+    FOREIGN KEY (user_id) REFERENCES "user"(user_id)
+);
+
+CREATE TABLE "product" (
+    product_id SERIAL PRIMARY KEY,
+    store_id INT NOT NULL,
+    product_name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price INT NOT NULL,
+    stock INT NOT NULL DEFAULT 0,
+    main_image_path VARCHAR(255),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMP DEFAULT NULL,
+
+    FOREIGN KEY (store_id) REFERENCES "store"(store_id)
+);
+
+CREATE TABLE "category" (
+    category_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE "category_item" (
+    category_id INT NOT NULL,
+    product_id INT NOT NULL,
+
+    PRIMARY KEY (category_id, product_id),
+    FOREIGN KEY (category_id) REFERENCES "category"(category_id),
+    FOREIGN KEY (product_id) REFERENCES "product"(product_id)
+);
+
+CREATE TABLE "cart_item" (
+    cart_item_id SERIAL PRIMARY KEY,
+    buyer_id INT NOT NULL, 
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+
+    FOREIGN KEY (buyer_id) REFERENCES "user"(user_id),
+    FOREIGN KEY (product_id) REFERENCES "product"(product_id)
+);
+
+CREATE TABLE "order" (
+    order_id SERIAL PRIMARY KEY,
+    buyer_id INT NOT NULL,
+    store_id INT NOT NULL,
+    total_price INT NOT NULL,
+    shipping_address TEXT NOT NULL,
+    status order_status NOT NULL DEFAULT 'waiting_approval',
+    reject_reason TEXT,
+    confirmed_at TIMESTAMP,
+    delivery_time TIMESTAMP,
+    received_at TIMESTAMP,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (buyer_id) REFERENCES "user"(user_id),
+    FOREIGN KEY (store_id) REFERENCES "store"(store_id)
+);
+
+CREATE TABLE "order_items" (
+    order_item_id SERIAL PRIMARY KEY,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    price_at_order INT NOT NULL, 
+    subtotal INT NOT NULL,
+
+    FOREIGN KEY (order_id) REFERENCES "order"(order_id),
+    FOREIGN KEY (product_id) REFERENCES "product"(product_id)
+);
+
+INSERT INTO "category" (name) VALUES
+('Rumah Tangga'),
+('Buku'),
+('Dapur'),
+('Elektronik'),
+('Mainan'),
+('Fashion Anak & Bayi'),
+('Fashion Pria'),
+('Fashion Wanita'),
+('Film dan Musik'),
+('Gaming'),
+('Handphone & Tablet'),
+('Ibu & Bayi'),
+('Kecantikan'),
+('Kesehatan'),
+('Mainan & Hobi'),
+('Komputer & Laptop'),
+('Office & Stationery'),
+('Olahraga'),
+('Otomotif'),
+('Perawatan Tubuh'),
+('Perawatan Hewan'),
+('Lainnya');
