4f83884259eb3c4d18f67c00b7940eb5a82c6c00
Muhammad Kinan Arkansyaddad <mkinanarkansyaddad@gmail.com>
2025-11-02 21:35:15 +0700

    feat: add store logo and description to product detail page

--- src/views/pages/product_detail.php
+++ src/views/pages/product_detail.php
@@ -37,15 +37,24 @@ use App\Core\Auth;
                 <div class="store-info">
                     <div class="store-header">
                         <div class="store-avatar">
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                                <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                                <polyline points="9 22 9 12 15 12 15 22"></polyline>
-                            </svg>
+                            <?php if (!empty($store->store_logo_path)): ?>
+                                <img 
+                                    src="<?php echo htmlspecialchars($store->getLogoPath()); ?>" 
+                                    alt="<?php echo htmlspecialchars($store->store_name); ?> Logo" 
+                                    class="store-logo-img"
+                                >
+                            <?php else: ?>
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                                    <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                                    <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                                </svg>
+                            <?php endif; ?>
                         </div>
                         <div class="store-details">
                             <h3 class="store-name" id="storeName"><?php echo htmlspecialchars($product->store_name)?></h3>
                         </div>
                     </div>
+                    <div class="store-description"><?php echo ($store->store_description) ?></div>
                 </div>
             </a>
 
