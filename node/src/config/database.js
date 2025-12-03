f56d06d067d87c670d0394bf730f37836891432e
Muhammad Kinan Arkansyaddad <mkinanarkansyaddad@gmail.com>
2025-12-03 08:49:42 +0700

    chore: setup nodejs

---
+++ node/src/config/database.js
@@ -0,0 +1,15 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: 5432,
+});
+
+pool.on('connect', () => {
+  console.log('Connected to the PostgreSQL database');
+});
+
+module.exports = pool;
