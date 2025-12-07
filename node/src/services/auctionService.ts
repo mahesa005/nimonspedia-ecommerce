80f0deed9680e767e3d06ef19ce10539bc8ed4e7
Muhammad Kinan Arkansyaddad <mkinanarkansyaddad@gmail.com>
2025-12-08 00:43:05 +0700

    feat: create placeBid service

--- node/src/services/auctionService.ts
+++ node/src/services/auctionService.ts
@@ -1,4 +1,4 @@
-import { AuctionDetailData, AuctionDetailResponse } from '../models/auctionModel';
+import { AuctionDetailData, AuctionDetailResponse, BidData } from '../models/auctionModel';
 import { AuctionRepository } from '../repositories/auctionRepository';
 
 export const AuctionService = {
@@ -13,5 +13,18 @@ export const AuctionService = {
       auction: auctionData,
       bids: bids
     };
+  },
+
+  async placeBid(auctionId: number, userId: number, amount: number): Promise<{bid: BidData, newEndTime: Date}> {
+    const auction = await AuctionRepository.findDetailById(auctionId);
+    if (!auction) throw new Error("Auction not found");
+
+    const minBid = parseInt(auction.current_price) + parseInt(auction.min_increment);
+    
+    if (amount < minBid && auction.bidder_count > 0) {
+      throw new Error(`Bid must be at least ${minBid}`);
+    }
+
+    return await AuctionRepository.createBid(auctionId, userId, amount);
   },
 };
