2376f3e50f73f8014c21f47b96048b9e9229dc03
Muhammad Kinan Arkansyaddad <mkinanarkansyaddad@gmail.com>
2025-12-08 12:42:52 +0700

    feat: update auction schema to add cancel support

--- database/db_schema.sql
+++ database/db_schema.sql
@@ -206,3 +206,12 @@ CREATE TABLE IF NOT EXISTS "user_feature_access" (
     updated_at TIMESTAMP DEFAULT NOW(),
     FOREIGN KEY (user_id) REFERENCES "user"(user_id)
 );
+
+ALTER TABLE "auctions"
+ADD COLUMN cancel_reason TEXT,
+ADD COLUMN cancelled_at TIMESTAMP;
+
+ALTER TABLE "auctions"
+ADD CONSTRAINT cancel_reason_required CHECK (
+    status != 'cancelled' OR cancel_reason IS NOT NULL
+);
