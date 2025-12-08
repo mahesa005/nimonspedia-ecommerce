d2dc9b3fbbe8ba73207f5da9a5988799feb61fb6
Mahesa Fadhillah Andre <167867383+mahesa005@users.noreply.github.com>
2025-12-08 21:28:29 +0700

    feat(admin): add userRepository getUsers function

--- node/src/repositories/userRepository.ts
+++ node/src/repositories/userRepository.ts
@@ -12,3 +12,42 @@ export async function findUserByEmail(email: string): Promise<User | null> {
     if (result.rows.length == 0) return null;
     return result.rows[0] as User;
 }
+
+export async function getUsers(params: {
+    page: number;
+    limit: number;
+    search?: string;
+}) {
+    const offset = (params.page - 1) * params.limit; // count offset
+    const result = await pool.query(
+        `SELECT * FROM "user" 
+        WHERE ($1 IS NULL OR name ILIKE $1 OR email ILIKE $1)
+        ORDER BY created_at DESC
+        OFFSET $2
+        LIMIT $3`,
+        [
+            params.search ? `%${params.search}%`: null, 
+            offset, 
+            params.limit
+        ]
+    )
+
+     const countRow = await pool.query(
+        `SELECT COUNT(*) AS total FROM users
+        WHERE ($1 IS NULL OR name ILIKE $1 OR email ILIKE $1)`,
+        [params.search ? `%${params.search}%` : null]
+    );
+    const total = Number(countRow.rows[0].total);
+    const totalPages = Math.ceil(total / params.limit);
+
+    return {
+        users: result.rows,
+        pagination: {
+            page: params.page,
+            limit: params.limit,
+            total,
+            totalPages,
+        }
+    }
+
+}
