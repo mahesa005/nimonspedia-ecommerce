dcebfd6bd2a5d88ec4f4bfb24af974f4a027fa8b
Sebastian Enrico Nathanael <sebastian230405@gmail.com>
2025-12-11 14:52:02 +0700

    refactor: backend auction to node.js

--- node/src/routes/auctionRoutes.ts
+++ node/src/routes/auctionRoutes.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express';
-import { cancelAuction, getAuctionDetail, stopAuction } from '../controllers/auctionController';
+import { cancelAuction, getAuctionDetail, stopAuction, getAuctions } from '../controllers/auctionController';
 import { verifySellerAuction } from '../middleware/verifySeller';
 import { requireAuth } from '../middleware/requireSession';
 
 const router = Router();
 
+router.get('/', getAuctions);
 router.get('/:id', getAuctionDetail);
 
-router.post('/:id/cancel', requireAuth,verifySellerAuction, cancelAuction);
+router.post('/:id/cancel', requireAuth, verifySellerAuction, cancelAuction);
 
-router.post('/:id/stop', requireAuth,verifySellerAuction, stopAuction);
+router.post('/:id/stop', requireAuth, verifySellerAuction, stopAuction);
 
 export default router;
