6947d46cb929daa96271a80b6694fd1996d31eca
Muhammad Kinan Arkansyaddad <mkinanarkansyaddad@gmail.com>
2025-12-11 14:41:07 +0700

    feat: add api to get information on navbar

--- node/src/routes/userRoutes.ts
+++ node/src/routes/userRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { requireAuth } from '../middleware/requireSession';
 import { getMe } from '../controllers/userController';
+import { getNavbarData } from '../controllers/navbarController';
 
 const router = Router();
 
 router.get('/me', requireAuth, getMe);
+router.get('/me/navbar', requireAuth, getNavbarData);
 
 export default router;
